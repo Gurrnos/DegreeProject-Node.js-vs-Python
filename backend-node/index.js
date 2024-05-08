a63ead71e579d44ec4461a9a75a6031912702fd0
Gurrnos <gurrnos01@gmail.com>
2024-05-08 13:03:24 +0200

    backend setup

--- backend-node/index.js
+++ backend-node/index.js
@@ -1,13 +1,19 @@
 const jwt = require('jsonwebtoken');
 const express = require('express');
 const app = express();
+const db = require("better-sqlite3")("node-database.db");
 
-
+app.use(
+  require("cors")({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(8080, () =>
+  console.log("Server is running on http://localhost:" + 8080)
+);
