7c6a7add18db55c8ae553a8943926d955b5058c9
4CEspX <simon@jonassonmail.se>
2024-05-08 13:48:09 +0200

    Commit nr something

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 const Login = () => {
+    const [userData, setUserData] = useState({
+        username: "",
+        password: "",
+    })
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setUserData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+  function Login(e) {
+    const data = {
+        ...userData
+    }
+  }
+
   return (
     <>
-      <div id="wrapper"></div>
+      <div>
+        <form onSubmit={Login}>
+          <input type="text" id="username" name="username" required onChange={handleChange}/>
+          <input type="password" id="password" name="password" required onChange={handleChange}/>
+          <input type="submit" value="Login"/>
+        </form>
+      </div>
     </>
   );
 };
